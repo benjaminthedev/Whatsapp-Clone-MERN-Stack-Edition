4e9ddab3339452000ff44d9c61b11edd81973b7d
Benjamin Dordoigne <ben.dordoigne@gmail.com>
2020-09-29 10:28:23 +0100

    updated project

--- whatsapp/src/Sidebar.js
+++ whatsapp/src/Sidebar.js
@@ -3,13 +3,14 @@ import "./Sidebar.css"
 import DonutLargeIcon from '@material-ui/icons/DonutLarge';
 import CommentIcon from '@material-ui/icons/Comment';
 import MoreVertIcon from '@material-ui/icons/MoreVert';
+import SearchOutlined from '@material-ui/icons/SearchOutlined';
 import { Avatar, IconButton } from '@material-ui/core'
 
 function Sidebar() {
     return (
         <div className="sidebar">
             <div className="sidebar__header">
-                <Avatar />
+                <Avatar src="https://avatars2.githubusercontent.com/u/14097672?s=460&u=8cf1c57db7771c88a5129eb682f1833f373c8f66&v=4" alt="avatar" />
                 <div className="sidebar__headerRight">
 
                     <IconButton>
@@ -25,7 +26,17 @@ function Sidebar() {
                     </IconButton>
                 </div>
             </div>
-        </div >
+
+
+
+            <div className="sidebar__search">
+                <div className="sidebar__searchContainer">
+                    <SearchOutlined />
+                    <input placeholder="Start Chat" type="text" />
+                </div>
+            </div>
+
+        </div>
     )
 }
 
