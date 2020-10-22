88ae03a90dd98534fca50635d1414c6c02e79b0e
Benjamin Dordoigne <ben.dordoigne@gmail.com>
2020-10-22 09:27:03 +0100

    Updated project

--- whatsapp/src/App.js
+++ whatsapp/src/App.js
@@ -1,10 +1,25 @@
-import React from 'react';
+import React, { useEffect } from 'react';
 import './App.css';
-import Chat from './Chat/Chat'
+import Chat from './Chat/Chat';
 import Sidebar from './SideBar/Sidebar';
+import Pusher from 'pusher-js';
 
 
 function App() {
+  useEffect(() => {
+    // Pusher
+    const pusher = new Pusher('1b48845d8bd43142c41e', {
+      cluster: 'eu'
+    });
+
+    const channel = pusher.subscribe("messages");
+    channel.bind("inserted", (data) => {
+      alert(JSON.stringify(data));
+      //console.log(JSON.stringify(data));
+    });
+  }, []);
+
+
   return (
     <div className="app">
       <div className="app__body">
